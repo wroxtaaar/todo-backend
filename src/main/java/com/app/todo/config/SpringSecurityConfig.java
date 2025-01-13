f98db98a0d14a8d6c90614e83dcaa489395df33c
wasiq <wroxtaar@gmail.com>
2025-01-13 21:57:20 +0530

    method level authorization in controller file with @PreAuthorize and @EnableMethodSecurity annotation

--- src/main/java/com/app/todo/config/SpringSecurityConfig.java
+++ src/main/java/com/app/todo/config/SpringSecurityConfig.java
@@ -3,7 +3,7 @@ package com.app.todo.config;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+// import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
