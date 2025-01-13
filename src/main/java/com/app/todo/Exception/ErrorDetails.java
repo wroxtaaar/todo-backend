7043114c47382dd5407c779ae37d20b199a329dd
wasiq <wroxtaar@gmail.com>
2025-01-14 01:00:10 +0530

    user already exists exception

---
+++ src/main/java/com/app/todo/Exception/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.app.todo.Exception;
+
+
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorDetails {
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
